81de6c348fc99431cfb0dd6b3028b3eab68c23f7
mandywwl <118342592+mandywwl@users.noreply.github.com>
2025-07-24 04:45:26 +0800

    test: npz test

---
+++ test/test_npz.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+data = np.load('../data/tgn_edges_basic.npz', allow_pickle=True)
+print("src:", data['src'])
+print("dst:", data['dst'])
+print("t:", data['t'])
+print("edge_attr:", data['edge_attr'])
+print("node_map:", data['node_map'])
