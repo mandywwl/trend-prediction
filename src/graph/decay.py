f160280a76294dd63bd994413a6b23d957a96854
mandywwl <118342592+mandywwl@users.noreply.github.com>
2025-08-28 02:46:13 +0800

    chore: decay logic update

--- src/graph/decay.py
+++ src/graph/decay.py
@@ -9,5 +9,6 @@ def apply_time_decay(ts, reference_time, decay_factor=0.5):
     """
     if isinstance(reference_time, str):
         reference_time = datetime.fromisoformat(reference_time)
-    delta_hours = (ts - reference_time).total_seconds() / 3600.0
+    
+    delta_hours = (reference_time - ts).total_seconds() / 3600.0
     return round(decay_factor ** delta_hours, 4)
